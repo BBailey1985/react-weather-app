351a457a0091a6f48975bbbb5a2a549c3227c40b
BBailey1985 <brandon_bailey@hotmail.com>
2022-08-15 13:32:09 -0500

    initial commit and Search component added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from './components/Search/Search';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+     <Search />
+    </div>
+  );
+}
+
+export default App;
