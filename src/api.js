55b33e2a848fbb09f9894cb65f2880f726797bf2
BBailey1985 <brandon_bailey@hotmail.com>
2022-08-15 14:28:16 -0500

    added GeoCity API and fetch functionality

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8a0dd52a70msh7962541600a388cp11fc28jsnf015ab7d1efa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
